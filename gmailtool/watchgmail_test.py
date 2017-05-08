a9392f362c6556be4a0d3956fb061f7508289dcf
Adam Terrey <arterrey@gmail.com>
2017-05-08 14:30:08 +1000

    Rename the mock gmail discovery json data file

--- gmailtool/watchgmail_test.py
+++ gmailtool/watchgmail_test.py
@@ -13,7 +13,7 @@ from gmailtool import watchgmail
 
 def get_gmail_api_descovery_json():
     this_dir = os.path.dirname(__file__)
-    json_path = os.path.join(this_dir, 'watchgmail_test_descovery.json')
+    json_path = os.path.join(this_dir, 'mock_gmail_discovery.json')
     fin = open(json_path, 'r')
     data = fin.read()
     return data
