bc3607a4e6c00e8a25d3fb787fd098a65342dbaa
Adam Terrey <arterrey@gmail.com>
2017-05-08 14:19:38 +1000

    Add basic python egg setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+"""setup.py script for gmailtool"""
+
+from setuptools import setup, find_packages
+
+setup(name='gmailtool',
+      version='1.0.dev1',
+      description='Command line tool for interacting with gmail',
+      long_description = open('README.rst').read(),
+      classifiers=[
+          'Topic :: Communications :: Email', 
+          'Programming Language :: Python',
+      ],
+      keywords='',
+      author='Adam Terrey',
+      author_email='software@adamandpaul.biz',
+      url='https://github.com/adamandpaul/gmailtool',
+      licence='gpl',
+      packages=['gmailtool'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'python-dateutil',
+          'google-api-python-client',
+      ],
+      entry_points="""
+      [console_scripts]
+      """,
+      )
+
+
