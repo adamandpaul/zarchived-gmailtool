608b9c9a016e77b06900f55e1aed4302d54c0de5
Adam Terrey <arterrey@gmail.com>
2017-05-12 17:04:52 +1000

    Complete implemntation of auth

--- gmailtool/config.py
+++ gmailtool/config.py
@@ -1,12 +1,19 @@
 """Release Level Configuration
 """
 
-import gmailtool.auth
+oauth_application_name = 'gmailtool'
 
-command_infos = [
-    {
-        'command': 'auth',
-        'help': 'Authenticate and save credentials for future invocations of gmailtool',
-        'register_function': gmailtool.auth.cmd_auth_register,
-    },
-]
+oauth_client_secret = {
+    "installed": {
+        "client_id": "112272676137-3ijeet0qkkgctotcbj1nc100l3roal29.apps.googleusercontent.com",
+        "project_id": "non-billable",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://accounts.google.com/o/oauth2/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_secret": "yrD6BRFRUaKsuQMT26spEGZp","redirect_uris": ["urn:ietf:wg:oauth:2.0:oob", "http://localhost"]
+    }
+}
+
+oauth_scopes = ['https://www.googleapis.com/auth/gmail.readonly']
+
+oauth_credentials_storage_filename =  'credentials.json'
--- gmailtool/main.py
+++ gmailtool/main.py
@@ -1,31 +1,22 @@
 """Main entry point for gmailtool"""
 
-import argparse
 import logging
 import os
 import sys
+import gmailtool.argument_parser
 
-from gmailtool import config
 
 logger = logging.getLogger('gmailtool')
 
-def ensure_profile_dir_exists(profile_dir):
-    """Make sure the profile dir for the command exists
-    
-    Args:
-        profile_dir (str); The path of the profile dir
-    """
-    profile_dir = os.path.expanduser(profile_dir)
-    if not os.path.exists(profile_dir):
-        logger.debug('Creating profile directory: ' + profile_dir)
-        os.makedirs(profile_dir)
-    logger.debug('Profile directory: ' + profile_dir)
-
 
 def configure_logging(verbosity):
-    """Configure logging
+    """Reconfigure logging with selected verbosity
+    
+    Sets the root logger and updates the args so oauth logging
+    will also be configured properly
     
     Args:
+        args (Object): Application args
         verbosity (int): The logging veribisty...
             0: WARN
             1: INFO
@@ -41,52 +32,32 @@ def configure_logging(verbosity):
     logging.basicConfig(level=level)
 
 
-def parse_args(argv, environ, command_infos):
-    """Parse command line and environment variable arguments
+def ensure_profile_dir_exists(profile_dir):
+    """Make sure the profile dir for the command exists
     
     Args:
-        argv (list of strings): The arguments to parse
-        environ (dict): The executable environment variables
-        command_infos (list of dicts): A list of dictionaries outlining command information
-    
-    Returns:
-        object: Object with properties of parse executable arguments
+        profile_dir (str); The path of the profile dir
     """
-    args = argparse.Namespace()
-    parser = argparse.ArgumentParser(description='Command line tool for fetching messages from your Gmail inbox',
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
-    # No-Option Arguments
-    args.executable = argv[0]
-    args.executable_name = os.path.basename(args.executable)
-
-    # Global Arguments
-    default_profile_dir = environ.get('PROFILE_DIR')
-    if default_profile_dir is None:
-        assert len(args.executable_name) > 0, 'No profile-dir is specified and executable name is missing or zero length'
-        default_profile_dir = '~/.' + args.executable_name
-    parser.add_argument('--profile-dir', default=default_profile_dir, help='The directory to store persistant data')
-    parser.add_argument('--verbose', '-v', action='count', help='Increase logging verbosity')
-
-    # Command Level Arguments
-    sub_parsers = parser.add_subparsers(title='command', help='gmailtool subcommands')
-    for command_info in command_infos:
-        sub_parser = sub_parsers.add_parser(command_info['command'], help=command_info['help'])
-        command_info['register_function'](sub_parser, environ)
-
-    # Parse and Return
-    parser.parse_args(argv[1:], namespace=args)
-    return args
+    profile_dir = os.path.expanduser(profile_dir)
+    if not os.path.exists(profile_dir):
+        logger.debug('Creating profile directory: ' + profile_dir)
+        os.makedirs(profile_dir)
+    logger.debug('Profile directory: ' + profile_dir)
 
 
-def main(argv=None, environ=None, command_infos=None):
+def main(argv=None, environ=None):
     """Main script entry point for gmailtool
     """
+
+    # Get Arguments
     argv = argv or sys.argv
     environ = environ or os.environ
-    command_infos = command_infos or config.command_infos
-    args = parse_args(argv, environ, command_infos)
+    args = gmailtool.argument_parser.parse_args(argv, environ)
+
+    # Run Initialization Code
     configure_logging(args.verbose)
     ensure_profile_dir_exists(args.profile_dir)
+
+    # Execute sub command
     args.func(args)
 
